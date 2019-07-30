a2330ab481088374ceb0a45f766a65d26b63ad4a
skarabocu <skara@umich.edu>
2019-07-30 11:29:38 -0400

    Put checkings to the computed values in movie card, so a new movie card can be created without throwing an error.

--- cards/movie/addon/components/embedded.js
+++ cards/movie/addon/components/embedded.js
@@ -5,12 +5,15 @@ import { computed } from '@ember/object';
 export default Component.extend({ 
     layout,
     genre: computed('content.genre', function() {
-        if(this.content.genre === 'Sci-Fi'){
+        if(this.content.genre === undefined) {
+            return '';
+        }
+        else if(this.content.genre === 'Sci-Fi'){
             return "sci-fi";
         }
         return this.content.genre.charAt(0).toLowerCase() + this.content.genre.slice(1);
     }),
     nowPlaying: computed('content.playing', function() {
-        return this.content.playing;
+        return this.content.playing ? this.content.playing : false;
     })
  });
