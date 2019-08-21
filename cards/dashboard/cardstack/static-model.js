79d0827ae44bf8a8aad644b7fa726eb58b2622ee
Jen Weber <j@jenweber.me>
2019-08-21 16:31:42 -0400

    stashing work

--- cards/dashboard/cardstack/static-model.js
+++ cards/dashboard/cardstack/static-model.js
@@ -3,25 +3,21 @@ const JSONAPIFactory = require('@cardstack/test-support/jsonapi-factory');
 let factory = new JSONAPIFactory();
 factory.addResource('content-types', 'dashboards')
   .withAttributes({
-    defaultIncludes: ['photos'],
+    defaultIncludes: ['photos', 'entries'],
     fieldsets: {
       isolated: [
-        {field: 'photos', format: 'embedded'}
+        {field: 'photos', format: 'embedded'},
+        {field: 'entries', format: 'embedded'}
       ]
     }
   })
   .withRelated('fields', [
+    {type: 'fields', id: 'title'},
     factory.addResource('fields', 'heading').withAttributes({
       fieldType: '@cardstack/core-types::string'
     }),
 
-    factory.addResource('fields', 'photos').withAttributes({
-      fieldType: '@cardstack/core-types::has-many',
-      editorComponent: 'field-editors/dropdown-search-multi-select-editor',
-    })
-    .withRelated('related-types', [{ type: 'content-types', id: 'photos' }]),
-
-    factory.addResource('fields', 'photos').withAttributes({
+    factory.addResource('fields', 'entries').withAttributes({
       fieldType: '@cardstack/core-types::has-many',
       editorComponent: 'field-editors/dropdown-search-multi-select-editor',
     })
