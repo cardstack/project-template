40daee76f49eb7dc07b79d9056f0a55f95ee46b4
Jen Weber <j@jenweber.me>
2019-09-04 11:58:18 -0400

    fix type error for null genre

--- cards/movie/addon/components/embedded.js
+++ cards/movie/addon/components/embedded.js
@@ -2,18 +2,16 @@ import Component from '@ember/component';
 import layout from '../templates/embedded';
 import { computed } from '@ember/object';
 
-export default Component.extend({ 
-    layout,
-    genre: computed('content.genre', function() {
-        if (this.content.genre === undefined) {
-            return '';
-        }
-        else if (this.content.genre === 'Sci-Fi') {
-            return "sci-fi";
-        }
-        return this.content.genre.charAt(0).toLowerCase() + this.content.genre.slice(1);
-    }),
-    nowPlaying: computed('content.playing', function() {
-        return this.content.playing ? this.content.playing : false;
-    })
- });
+export default Component.extend({
+  layout,
+  genre: computed('content.genre', function() {
+    if (!this.content.genre) {
+      return 'other';
+    } else {
+      return this.content.genre.toLowerCase()
+    }
+  }),
+  nowPlaying: computed('content.playing', function() {
+      return this.content.playing ? this.content.playing : false;
+  })
+});
